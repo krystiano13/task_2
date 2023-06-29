caf1a31b836aa3692bd3f6d93caf7277fbd2e5ad
krystiano13 <crystianotv@gmail.com>
2023-06-29 14:32:08 +0200

    search input for mobile version

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const hamburgerMenu = document.querySelector('.hamburgermenu');
+const searchButton = document.querySelector('.searchButton');
 const navbar = document.querySelector('nav');
+const searchForm = document.querySelector('.searchForm');
 
 hamburgerMenu.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle('crossedStripes');
+    hamburgerMenu.classList.toggle("crossedStripes");
     navbar.classList.toggle('shown');
+    searchForm.classList.remove('searchForm-visible');
+});
+
+searchButton.addEventListener('click', () => {
+    hamburgerMenu.classList.remove("crossedStripes");
+    navbar.classList.remove("shown");
+    searchForm.classList.toggle("searchForm-visible");
 });
